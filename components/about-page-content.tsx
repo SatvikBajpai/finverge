ff34f40751571ee4c171aaf926a1da70ef845611
Satvik Bajpai <satvik.bajpai@plaksha.edu.in>
2025-07-12 17:13:42 +0530

    Update about page content and testimonials

--- components/about-page-content.tsx
+++ components/about-page-content.tsx
@@ -144,7 +144,7 @@ export function AboutPageContent() {
           >
             <div className="flex-shrink-0 w-full md:w-1/3 flex justify-center mb-6 md:mb-0">
               <Image
-                src="/founder_image.jpg"
+                src="/founder_image.jpeg"
                 alt="Vishal Manwani, Founder of Finverge Advisors"
                 width={180}
                 height={180}
